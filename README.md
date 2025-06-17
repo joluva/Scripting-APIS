02f9d91ce7e0d4555bdc0a365e339e1922cec6ed
Jorge Luis Vasquez <jlv0362@gmail.com>
2025-06-17 19:48:44 -0300

    Primer commit de la API

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Scripting-APIS
 curso data science APIs de Scripting
+
+# Descripción
+
+Se encuentran las APIs correspondiente a la materia
+
+## Módulos que integran la materia
+
+### MOD-2 API-2
+    Mostrar el resultado de distintas variables luego de realizar operaciones matemáticas de suma, división y multiplicación
+    La suma se realiza con números enteros (10, 3) y se devuelve un float como resultado. En la división se realiza la operación con los mismos números (10, 3) y se devuelve como resultado un entero. La multiplicación se realiza con los número (5, 2) y se devuelve como float.
---
+++ api-2-scripting.py
@@ -0,0 +1,18 @@
+# API-2 MOD-2. Realizar operaciones numéricas
+# Sumar dentro de una variable y mostrar salida en formato float
+add_number=float(10 + 3)
+print("-------------------------------------------------------------------\n")
+print(f"La suma de los valores 10 + 3 da como resultado float: {add_number}\n")
+print("-------------------------------------------------------------------\n")
+
+# Calcular el cociente de 10 / 3 y mostrarlo como entero
+div_number=int(10 / 3)
+print("-------------------------------------------------------------------\n")
+print(f"La cociente entre 10 / 3 da como resultado int: {div_number}\n")
+print("-------------------------------------------------------------------\n")
+
+# Realizar el producto entre los números 5 * 2 y mostrarlo como float
+mult_number=float(5 * 2)
+print("-------------------------------------------------------------------\n")
+print(f"El producto entre 5 * 2 da como resultado float: {mult_number}\n")
+print("-------------------------------------------------------------------\n")
